5b0a023c095fc1a62783ab6ccb69f7e99b5b015b
pevargasg <wearetamo@gmail.com>
2018-12-02 10:46:50 -0800

    added signup form

--- src/app/auth/sigup/sigup.component.ts
+++ src/app/auth/sigup/sigup.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-sigup',
-  templateUrl: './sigup.component.html',
-  styleUrls: ['./sigup.component.css']
+  selector: "app-sigup",
+  templateUrl: "./sigup.component.html",
+  styleUrls: ["./sigup.component.css"]
 })
 export class SigupComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onSubmit(form: NgForm) {
+    console.log(form.value);
   }
-
 }
