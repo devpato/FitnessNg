1e7b5c40ed0072e71763af1f79349193865d9a63
pevargasg <wearetamo@gmail.com>
2018-12-02 09:31:41 -0800

    welcome component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,10 @@ import { LoginComponent } from './auth/login/login.component';
 import { CurrentTrainingComponent } from './training/current-training/current-training.component';
 import { NewTrainingComponent } from './training/new-training/new-training.component';
 import { PastTrainingComponent } from './training/past-training/past-training.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { TrainingComponent } from './training/training/training.component';
 @NgModule({
-  declarations: [AppComponent, SigupComponent, LoginComponent, CurrentTrainingComponent, NewTrainingComponent, PastTrainingComponent],
+  declarations: [AppComponent, SigupComponent, LoginComponent, CurrentTrainingComponent, NewTrainingComponent, PastTrainingComponent, WelcomeComponent, TrainingComponent],
   imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent]
