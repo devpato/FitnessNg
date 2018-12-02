ced779b9ae9c8179dadaf078731270ee8cfe79db
pevargasg <wearetamo@gmail.com>
2018-12-02 10:46:42 -0800

    added flex layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { PastTrainingComponent } from "./training/past-training/past-training.co
 import { WelcomeComponent } from "./welcome/welcome.component";
 import { TrainingComponent } from "./training/training/training.component";
 import { AppRoutingModule } from "./app.routing.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -27,7 +29,9 @@ import { AppRoutingModule } from "./app.routing.module";
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
