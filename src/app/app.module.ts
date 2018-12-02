2792e1f09b0a3a9650195db70ded10f185751700
pevargasg <wearetamo@gmail.com>
2018-12-02 09:26:24 -0800

    created current training component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,9 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialModule } from "./metrial.module";
 import { SigupComponent } from './auth/sigup/sigup.component';
 import { LoginComponent } from './auth/login/login.component';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
 @NgModule({
-  declarations: [AppComponent, SigupComponent, LoginComponent],
+  declarations: [AppComponent, SigupComponent, LoginComponent, CurrentTrainingComponent],
   imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent]
