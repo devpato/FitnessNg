06a776c5a0895a20f7d3c90f4a558bce3919ba94
pevargasg <wearetamo@gmail.com>
2018-12-02 09:25:38 -0800

    added login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,9 @@ import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialModule } from "./metrial.module";
 import { SigupComponent } from './auth/sigup/sigup.component';
+import { LoginComponent } from './auth/login/login.component';
 @NgModule({
-  declarations: [AppComponent, SigupComponent],
+  declarations: [AppComponent, SigupComponent, LoginComponent],
   imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent]
