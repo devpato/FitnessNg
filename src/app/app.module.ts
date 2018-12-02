44d809d5e307cd754638958f19efd95003761f7b
pevargasg <wearetamo@gmail.com>
2018-12-02 09:27:08 -0800

    added new training

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,9 @@ import { MaterialModule } from "./metrial.module";
 import { SigupComponent } from './auth/sigup/sigup.component';
 import { LoginComponent } from './auth/login/login.component';
 import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
 @NgModule({
-  declarations: [AppComponent, SigupComponent, LoginComponent, CurrentTrainingComponent],
+  declarations: [AppComponent, SigupComponent, LoginComponent, CurrentTrainingComponent, NewTrainingComponent],
   imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent]
