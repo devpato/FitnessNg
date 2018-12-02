e9177a05adcc35a5413c3e8927665e05d8c86f34
pevargasg <wearetamo@gmail.com>
2018-12-02 09:25:18 -0800

    auth component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,9 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialModule } from "./metrial.module";
+import { SigupComponent } from './auth/sigup/sigup.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SigupComponent],
   imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent]
