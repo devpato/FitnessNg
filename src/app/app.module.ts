ddc0ca80c9028f913ff04010f8c15cf3058f23ac
pevargasg <wearetamo@gmail.com>
2018-12-02 18:37:30 -0800

    header created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { TrainingComponent } from "./training/training/training.component";
 import { AppRoutingModule } from "./app.routing.module";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { FormsModule } from "@angular/forms";
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,9 @@ import { FormsModule } from "@angular/forms";
     NewTrainingComponent,
     PastTrainingComponent,
     WelcomeComponent,
-    TrainingComponent
+    TrainingComponent,
+    HeaderComponent,
+    SidenavListComponent
   ],
   imports: [
     BrowserModule,
