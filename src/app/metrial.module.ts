fe2e124448f0f7c390df59b8af63f9db2e6345bc
pevargasg <wearetamo@gmail.com>
2018-12-02 19:30:21 -0800

    tabs added

--- src/app/metrial.module.ts
+++ src/app/metrial.module.ts
@@ -8,6 +8,7 @@ import { MatCheckboxModule } from "@angular/material/checkbox";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatListModule } from "@angular/material/list";
+import { MatTabsModule } from "@angular/material/tabs";
 
 @NgModule({
   imports: [
@@ -32,7 +33,8 @@ import { MatListModule } from "@angular/material/list";
     MatCheckboxModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatListModule
+    MatListModule,
+    MatTabsModule
   ]
 })
 export class MaterialModule {}
