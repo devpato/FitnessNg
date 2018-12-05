74fd765c46ef454132c0477f7da873c85b0177ea
pevargasg <wearetamo@gmail.com>
2018-12-04 21:48:17 -0800

    added start new exercise

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,14 +1,28 @@
 import { Exercise } from "../models/excercise.model";
+import { Subject } from "rxjs/Subject";
 
 export class TrainingService {
-    available: Exercise[]
+    private excercises: Exercise[]
+    private runningExcercise: Exercise;
+    excerciseChanged = new Subject<Exercise>();
 
     buildExercises(): Exercise[] {
-        return this.available = [
+        return this.excercises = [
             { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
             { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
             { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
             { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
         ]
     }
+
+    getAvailableExcercise() {
+        return this.excercises.slice();
+    }
+
+    startExcercise(selectedId: string) {
+        this.runningExcercise = this.excercises.find(e => e.id === selectedId);
+        this.excerciseChanged.next({ ...this.runningExcercise });
+    }
+
+
 }
