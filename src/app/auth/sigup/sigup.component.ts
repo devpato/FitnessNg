400efb4a60ffa8383145e9d76c9f106a27af705b
pevargasg <wearetamo@gmail.com>
2018-12-04 18:55:37 -0800

    onsubmit signup page

--- src/app/auth/sigup/sigup.component.ts
+++ src/app/auth/sigup/sigup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { AuthService } from "../auth.service";
 
 @Component({
   selector: "app-sigup",
@@ -8,13 +9,16 @@ import { NgForm } from "@angular/forms";
 })
 export class SigupComponent implements OnInit {
   maxDate = new Date();
-  constructor() {}
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
     this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
+    this.authService.registerUser({
+      email: form.value.email,
+      password: form.value.password
+    })
   }
 }
