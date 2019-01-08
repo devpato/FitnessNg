9c9cd008c9175fdf033c42702a649e5116ca89bb
pevargasg <wearetamo@gmail.com>
2019-01-08 01:52:49 -0800

    using canLoad with AuthGuard

--- src/app/training/training/training-routing.module.ts
+++ src/app/training/training/training-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { TrainingComponent } from "./training.component";
-import { AuthGuard } from "src/app/auth/auth.guard";
 
 const routes: Routes = [
   //We used to have training in the path, but we remove it because the code below will
   //be added to the app-routing anyways.
-  { path: "", component: TrainingComponent, canActivate: [AuthGuard] }
+  { path: "", component: TrainingComponent }
 ];
 
 @NgModule({
